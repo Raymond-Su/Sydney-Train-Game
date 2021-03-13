7d72edc0146e315f74f07630501f910d11b640d9
Raymond Su <rayonixs@gmail.com>
2021-03-13 13:01:59 +1100

    refactor: removed mongoose

--- server/api/models/userScores.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const scoreSchema = mongoose.Schema({
-  _id: mongoose.Schema.Types.ObjectId,
-  name: { type: String, required: true },
-  score: { type: Number, required: true }
-});
-
-module.exports = mongoose.model('UserScore', scoreSchema);
--- server/server.js
+++ server/server.js
@@ -3,26 +3,7 @@ require('dotenv').config({
   path: path.join(__dirname, '.env')
 });
 const server = require('./app');
-const mongoose = require('mongoose');
 const apiPort = process.env.PORT || 5000;
-
-mongoose.connect(
-  'mongodb+srv://' +
-    `${process.env.MONGO_ATLAS_USERNAME}:` +
-    `${process.env.MONGO_ATLAS_PASSWORD}` +
-    `@node-rest-make10.akcrv.mongodb.net/` +
-    `${process.env.MONGO_ATLAS_DB}` +
-    `?retryWrites=true&w=majority`,
-  {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-  }
-);
-const connection = mongoose.connection;
-
-connection.once('open', function () {
-  console.log('MongoDB database connection established successfully');
-});
 
 server.listen(apiPort, () => {
   console.log(`Server running on port ${apiPort}`);
